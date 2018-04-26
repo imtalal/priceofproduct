1c73e33bf255d616f03fea6c45e69c5b5fd591d8
imtalal <talal.munir@gmail.com>
2018-04-26 22:09:33 +0500

    Add price.php file

---
+++ price.php
@@ -0,0 +1,24 @@
+<?php
+
+/* 
+ * An abstract class  ---- price calculation of different shape products
+ * Done by : Developer imtalal
+ */
+abstract class price {
+    
+    // abstract method for price
+    abstract function priceOfProduct();
+    // for area of rectangle
+    public function areaOfRectangle( $length, $width ){
+        
+    }
+    // for area of circle
+    public function areaOfCircle( $radius ){
+        
+    }
+    // for area of triangle
+    public function areaOfTriangle( $height, $base ){
+        
+    }
+}
+?>
