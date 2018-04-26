fce8050ee24d8ec51bb833946f216562b3be9d3f
imtalal <talal.munir@gmail.com>
2018-04-26 22:08:43 +0500

    Add index.php file

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ * index file  ---- price calculation of different shape products
+ * Done by : Developer imtalal
+ */
+
+class main {
+
+    public function __construct() {
+        // adding css file
+        echo '<link rel="stylesheet" href="style.css">';
+        // adding jquery file
+        echo '<script src="jquery-3.3.1.min.js"></script>';
+        // including ajax
+        echo '<script src="ajax_file.js"></script>';
+    }
+
+}
+
+// making ibject of main class
+$mainObj = new main();
+?>
+
+
+<div class='main'>
+    <section class="content">
+        <h1>Price Calculator</h1>
+        <p>
+            Please input required data.
+        </p>
+    </section>
+    <table class="heavyTable">
+        <thead>
+            <tr>
+                <th>Shape</th>
+                <th>Input</th>
+                <th>Input</th>
+                <th>Calculate</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Circle</td>
+                <td>Radius: <input required="" id="radID" border="1" type="text" name="radius" required></td>
+                <td>
+                </td>
+                <td><button id="radCalculate" name="radius" value="Calculate Price" >Calculate Price</button></td>
+            </tr>
+            <tr>
+                <td>Rectangle</td>
+                <td>Length: <input id="lenID" name="length" type="text" name="length" required>
+                </td>
+                <td>Width: <input id="widID" name="width" type="text" name="width"  required></td>
+                <td><button id="recCalculate" value="Calculate Price" >Calculate Price</button></td>
+            </tr>
+            <tr>
+                <td>Triangle</td>
+                <td>Height: <input id="heiID" name="height" type="text" name="height" required></td>
+                <td>Base: <input id="baseID" name="base" type="text" name="base" required></td>
+                <td><button id="triCalculate" value="Calculate Price" >Calculate Price</button></td>
+            </tr>
+        </tbody>
+    </table>
+
+</div>
